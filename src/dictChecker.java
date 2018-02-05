f2eb39d2db31dd776b4a81c10e78b08ce05cf3c1
Rettdog <30802926+Rettdog@users.noreply.github.com>
2018-02-04 17:00:37 -0800

    dictChecker2, dictChecker

--- src/dictChecker.java
+++ src/dictChecker.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 public class dictChecker {
 	public static void main(String[] args) throws FileNotFoundException {
 		PrintWriter pw = new PrintWriter(new File("dictionary.txt"));
-		Scanner scan = new Scanner(new File("dict.txt"));
-	
+		File file = new File("dict.txt");
+		Scanner sc = new Scanner(file);
+		String line;
 for(int i = 0;i<1;) {
-
+	line = sc.next("dict.txt");
+if(line.length()==4||line.length()==3) {
+	pw.println(sc.nextLine());
+}
 	
 }
 }
